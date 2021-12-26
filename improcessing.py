0f2a046bb6c56e6716f15cf837dfc47941495b9e
Pete Yeh <peteyeh8@gmail.com>
2021-12-25 21:06:57 -0800

    Add edge detection mask modes
    We currently obtain masks using pixel intensity but edge detection
    could theoretically allow for better identification of faint cells.
    Let's add functionality for edge detection with Canny and/or Sobel,
    with appropriate parameters included for adjustments. Unfortunately
    edge detection seems to perform poorly - it appears to be highly
    sensitive to the many edges around busy groups of cells and hinders
    our ability to differentiate small cells within those groups - so
    let's stick with histogram equalization as our method of choice for
    the time being.

--- improcessing.py
+++ improcessing.py
@@ -11,6 +11,9 @@ def apply_closure(image, kernel_size=5):
     kernel = np.ones((kernel_size,kernel_size),np.uint8)
     return cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
 
+def apply_denoise(image, h=3):
+    return cv2.fastNlMeansDenoising(image, h=h)
+
 def apply_imfill(image):
     t = image.copy()
     y_dim, x_dim = image.shape
@@ -20,6 +23,17 @@ def apply_imfill(image):
     t = cv2.floodFill(t, mask, (x_dim-1, 0), 255)[1]
     t = cv2.floodFill(t, mask, (x_dim-1, y_dim-1), 255)[1]
     return image | cv2.bitwise_not(t)
+
+def apply_sharpen(image):
+    kernel = np.array([[0, -1,  0],
+                       [-1, 5, -1],
+                       [0, -1,  0]])
+    # ddepth=-1 preserves the source depth
+    return cv2.filter2D(src=image, ddepth=-1, kernel=kernel)
+
+def apply_sobel(image, kernel_size=5):
+    # ddepth=-1 preserves the source depth
+    return cv2.Sobel(src=image, ddepth=-1, dx=1, dy=1, ksize=kernel_size)
 
 # connectivity: 4 to exclude diagonals, 8 to include
 def get_num_components(image, connectivity=8):
