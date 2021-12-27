eaff0d63e9411bfa77ca08defb85c112ac3ee37f
Pete Yeh <peteyeh8@gmail.com>
2021-12-27 14:04:26 -0800

    Add background filtering mask mode
    Let's try something simple for masking - filtering background pixel
    values. We can acquire a centered background grayscale value using
    the (very prominent) peak in the histogram. Using that peak, we can
    filter out pixel values within a certain range from that peak,
    effectively creating a mask by ignoring values within the range of
    the background.
    
    This works decently well but struggles in busier areas - our
    previous approach of thresholding after equalization seems to be
    superior, so let's stick with that.

--- displaytools.py
+++ displaytools.py
@@ -7,12 +7,14 @@ def display_image(image, dpi=240):
     plt.show()
     plt.close()
 
-def display_image_array(im_arr, columns=7, dpi=240):
+def display_image_array(im_arr, titles=None, columns=7, dpi=240):
     rows = int(len(im_arr) / columns) + 1
     plt.figure(dpi=dpi)
     for i in range(len(im_arr)):
         plt.subplot(rows,columns,i+1)
         plt.imshow(im_arr[i], interpolation='nearest', cmap='gray')
+        if titles and len(im_arr) == len(titles):
+            plt.title(titles[i], fontsize=5)
         plt.axis('off')
     plt.show()
     plt.close()
