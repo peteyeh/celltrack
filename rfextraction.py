peteyeh/celltrack

commit 99940d8273ed87ab89e51fc743ac701277e07b7a

Parameter search proof of concept
Given inherent background variation across each image stack we
should implement a way to intelligently adjust model parameters as
we step through the images. In particular, the offset parameter is
a good target for tweaking as the background can be a bit brighter
or darker depending on the image.

In this proof of concept, we sweep through a search space and track
mask image output with two metrics: the total area initially
captured and the number of unique cell regions that is in our final
mask output. We would ideally like to mask as much as possible
before the total area sharply increases (i.e. we start picking up
background) or the number of cell regions sharply decreases (i.e.
there isn't enough separation between unique regions).

We observe a clear uptick in total area as we increase the offset
though the number of cell regions appears to be more stable. Total
area thus may be a more useful tool for detecting appropriate
parameters.

rfextraction.py
@@ -42,11 +42,15 @@ def extract_features(image, mask_image):
     # masks - our sample order will match the labels in mask_labels
     return pd.DataFrame.from_dict(features).set_index(['x', 'y']), mask_labels
 
-def get_mask_image(image, param_path, verbosity=0):
-    with open(param_path, 'r') as f:
-        param_file = yaml.load(f, Loader=yaml.FullLoader)
-        mode = param_file['mode']
-        params = param_file['params']
+def get_mask_image(image, mask_params, verbosity=0):
+    if type(mask_params) == str:
+        with open(param_path, 'r') as f:
+            param_file = yaml.load(f, Loader=yaml.FullLoader)
+            mode = param_file['mode']
+            params = param_file['params']
+    else:
+        mode = mask_params['mode']
+        params = mask_params['params']
 
     if verbosity:
         print("Using mode '%s'." % mode)

commit 3e71f67c08a34e39f012821e235e7413202bef6d

Modify Canny unmasking logic and fix get_mask_image bug
Our prior method for unmasking via Canny edges was built on an
assumption that intensity-detected edges would be a strict subset
of a filled Canny image, which is a bit of a crude approach - there
are likely regions where edges are sparsely detected and thus by
&-ing the two images together we are more likely to get a patchier
mask.

Let's rework our approach to unmask after image closure, in the
same step as the size thresholding, and remove components if and
only if no edges are detected within the component.

Fix a get_mask_image variable naming bug, introduced in 99940d8.

This commit has been tested but is not reflected in the notebook.

rfextraction.py
@@ -44,7 +44,7 @@ def extract_features(image, mask_image):
 
 def get_mask_image(image, mask_params, verbosity=0):
     if type(mask_params) == str:
-        with open(param_path, 'r') as f:
+        with open(mask_params, 'r') as f:
             param_file = yaml.load(f, Loader=yaml.FullLoader)
             mode = param_file['mode']
             params = param_file['params']
@@ -158,11 +158,6 @@ def get_mask_image(image, mask_params, verbosity=0):
 
     ### IMAGE POSTPROCESSING ###
     
-    if params.get('canny_unmasking'):
-        if verbosity:
-            print("Unmasking areas without edges using Canny%s:" % params.get('canny_unmasking'))
-        binarized = binarized & canny_filled  # POSSIBLE BUG: CHECK IF CANNY_FILLED IS BINARY
-    
     if params.get('border_removal'):
         if verbosity:
             print("Removing border of %i pixels." % params['border_removal'])
@@ -178,14 +173,17 @@ def get_mask_image(image, mask_params, verbosity=0):
     else:
         filled = apply_imfill(binarized)
 
-    if params.get('size_thresh'):
+    if params.get('canny_unmasking') or params.get('size_thresh'):
         if verbosity:
-            print("Unmasking regions smaller than %i pixels." % params['size_thresh'])
+            if params.get('canny_unmasking'):
+                print("Unmasking areas without edges using Canny%s:" % params.get('canny_unmasking'))
+            if params.get('size_thresh'):
+                print("Unmasking regions smaller than %i pixels." % params['size_thresh'])
         _, labelled, stats, _ = cv2.connectedComponentsWithStats(filled, connectivity=8)
         for i in range(1, len(stats)):  # index 0 is the background component
-            size = stats[i, -1]
-            if size < params['size_thresh']:
-                filled = filled * (1 - np.uint8(labelled == i))
+            if (params.get('canny_unmasking') and np.sum(np.uint8(labelled==i)*canny_filled) == 0) \
+                or (params.get('size_thresh') and stats[i, -1] < params['size_thresh']):
+                    filled = filled * (1 - np.uint8(labelled == i))
 
     if verbosity >= 2:
         display_image_array([image, binarized, filled],