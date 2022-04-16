f5acedb2024aecb35c43a97ddb4bb6d984833357
Pete Yeh <peteyeh8@gmail.com>
2022-04-15 22:33:39 -0500

    Add scripts for Northwestern Quest
    quest_initialize.sh will create a conda environment and load all
    Python libraries necessary for this project.
    
    job-script.sh is a template script for launching jobs on Quest.

---
+++ quest_scripts/job-script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --account=b1042 
+#SBATCH --partition=genomics
+#SBATCH --nodes=1
+#SBATCH --n=52
+#SBATCH --time=00:40:00
+#SBATCH --mem-per-cpu=1G
+#SBATCH --job-name=image_analysis
+#SBATCH --output=outlog
+#SBATCH --error=errlog
+
+module purge all                ## Unload existing modules
+module load python/anaconda3.6
+
+source activate celltrack
+
---
+++ quest_scripts/quest_initialize.sh
@@ -0,0 +1,18 @@
+module purge all
+
+module load python/anaconda3.6
+
+conda create --name celltrack python=3.7.11 --yes
+
+source activate celltrack
+
+conda install -y -c conda-forge matplotlib=3.5.0
+conda install -y -c fastai opencv-python-headless=4.5.3.56
+conda install -y pandas=1.3.4
+conda install -y -c conda-forge pyyaml=6.0
+conda install -y -c conda-forge scikit-learn=1.0.1
+conda install -y -c conda-forge tqdm=4.62.3
+
+pip install --no-input pyradiomics==3.0.1
+pip install --no-input scikit-image==0.19.1
+
