peteyeh/celltrack

commit 7441c7a5727bd419de884ef677e5ff42d0beb46e

Standardize output pathing
In this change we stop using __temp files for generated output.
Instead, scripts can take a path to an output directory and create
timed entries for each image stack of interest. Generally speaking,
the directory structure is:
/[output directory]/[image stack]/[timestamp]/[output file(s)]

For output specific to individual image stacks, timestamps are
propagated from mask image creation - subsequent scripts will look
for the latest generated mask images.

Since kmeans output does not run on a similar timeline (we can fit
models using several image stacks), the pathing is a bit different:
/[output directory]/[kmeans/silhouette_scores]/[timestamped file]

Note that this code is presently untested as it is intended to be
run on Quest.

src/kmeans_initialize.py
@@ -4,6 +4,7 @@ import pandas as pd
 import pickle
 import sys
 
+from datetime import datetime
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.metrics import silhouette_score
@@ -14,9 +15,13 @@ if __name__ == "__main__":
     with open(os.path.basename(sys.argv[1]), "rb") as infile:
         image_paths = map(lambda a: a.decode(), infile.read().splitlines())
 
+    out_path = "." if len(sys.argv) < 3 else sys.argv[2]  # this directory should already exist
+
     full_df = pd.DataFrame()
     for p in image_paths:
-        feature_path = "__temp-" + os.path.basename(p).split('.')[0] + "-extracted_features.pickle"
+        base_path = os.path.join(out_path, os.path.basename(p).split('.')[0])
+        last_run = sorted(os.listdir(base_path), reverse=True)[0]
+        feature_path = os.path.join(base_path, last_run, "extracted_features.pickle")
         print("Loading features from %s." % feature_path)
         with open(feature_path, "rb") as infile:
             for df, _ in pickle.load(infile):
@@ -40,7 +45,7 @@ if __name__ == "__main__":
         kmeans = KMeans(n_clusters=k, random_state=0)
         kmeans.fit(dft)
 
-        write_path = "trained-kmeans.pickle"
+        write_path = os.path.join(out_path, "kmeans", datetime.now().strftime("%Y%m%d%H%M%S") + ".pickle")
         print("Writing trained models to " + write_path + ".")
         with open(write_path, "wb") as outfile:
             pickle.dump([scaler, pca, kmeans], outfile)
@@ -56,4 +61,4 @@ if __name__ == "__main__":
         plt.plot(k_range, silhouette_scores,'bx-')
         plt.title("Silhouette Scores")
         plt.xlabel("k")
-        plt.savefig("silhouette_scores.png")
+        plt.savefig(os.path.join(out_path, "silhouette_scores", datetime.now().strftime("%Y%m%d%H%M%S") + ".png"))

src/kmeans_predict.py
@@ -4,13 +4,19 @@ import pickle
 import sys
 
 if __name__ == "__main__":
+    out_path = "." if len(sys.argv) < 3 else sys.argv[2]  # this directory should already exist
+
     try:
-        sub_name = os.path.basename(sys.argv[1]).split('.')[0]
-        feature_path = "__temp-" + sub_name + "-extracted_features.pickle"
-        kmeans_path = "trained-kmeans.pickle"
+        base_path = os.path.join(out_path, "kmeans")
+        last_run = sorted(os.listdir(base_path), reverse=True)[0]
+        kmeans_path = os.path.join(base_path, last_run + ".pickle")
+
+        base_path = os.path.join(out_path, os.path.basename(sys.argv[1]).split('.')[0])
+        last_run = sorted(os.listdir(base_path), reverse=True)[0]
+        feature_path = os.path.join(base_path, last_run, "extracted_features.pickle")
     except:
         print("Received invalid image_path. Make sure you execute with:")
-        print("  python3 kmeans_predict.py [image_path]")
+        print("  python3 kmeans_predict.py image_path [output_path]")
         sys.exit(1)
 
     try:
@@ -34,8 +40,7 @@ if __name__ == "__main__":
         df = pd.DataFrame(pca.transform(scaler.transform(df)), index=df.index)
         labels += [kmeans.predict(df),]
 
-    write_path = "__temp-" + sub_name + "-labels.pickle"
+    write_path = os.path.join(base_path, last_run, "labels.pickle")
     print("Writing labels to " + write_path + ".")
     with open(write_path, "wb") as outfile:
         pickle.dump(labels, outfile)
-

src/maskcreation.py
@@ -201,6 +201,7 @@ def get_mask_image_with_refined_offset(image, mask_params=default_params, left=5
 
 if __name__ == "__main__":
     import imageio, os, shutil, sys
+    from datetime import datetime
     from joblib import cpu_count, delayed, Parallel
     from tqdm import tqdm
 
@@ -213,7 +214,7 @@ if __name__ == "__main__":
             raise Exception()
     except:
         print("Unable to read image stack. Make sure you execute with:")
-        print("  python3 maskcreation.py [image_path]")
+        print("  python3 maskcreation.py image_path [output_path]")
         sys.exit(1)
 
     print("Creating %i mask images:" % len(image_stack))
@@ -221,10 +222,14 @@ if __name__ == "__main__":
         Parallel(n_jobs=cpu_count())(
             delayed(get_mask_image_with_refined_offset)(_) for _ in tqdm(image_stack))
 
-    write_path = "__temp-" + os.path.basename(sys.argv[1]).split('.')[0] + "-mask_images"
+    out_path = "." if len(sys.argv) < 3 else sys.argv[2]  # this directory should already exist
+    base_path = os.path.join(outpath, os.path.basename(sys.argv[1]).split('.')[0])
+    if not os.path.exists(base_path):
+        print("Creating base directory %s." % base_path)
+        os.mkdir(base_path)
+    write_path = os.path.join(base_path, datetime.now().strftime("%Y%m%d%H%M%S"), "mask_images")
     print("Writing mask images to " + write_path + ":")
-    if os.path.exists(write_path):
-        shutil.rmtree(write_path)
+    os.mkdir(os.path.split(write_path)[0])
     os.mkdir(write_path)
     for i in tqdm(range(len(mask_images))):
         imageio.imwrite((write_path + "/%i.png" % i), mask_images[i])

reference/job-script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account=b1042
 #SBATCH --partition=genomics
-#SBATCH --ntasks-per-node=52
-#SBATCH --time=00:40:00
+#SBATCH --ntasks-per-node=50
+#SBATCH --time=08:00:00
 #SBATCH --mem-per-cpu=1G
 #SBATCH --job-name=image_analysis
 #SBATCH --output=outlog
@@ -13,3 +13,6 @@ module load python/anaconda3.6
 
 source activate celltrack
 
+output="/projects/b1042/Abazeed_Lab/Pete_Priyanka/output"
+
+cd ~/celltrack/src

src/ftextraction.py
@@ -64,11 +64,15 @@ if __name__ == "__main__":
         image_stack = cv2.imreadmulti(sys.argv[1], flags=cv2.IMREAD_GRAYSCALE)[1]
     except:
         print("Unable to read image stack. Make sure you execute with:")
-        print("  python3 ftextraction.py [image_path]")
+        print("  python3 ftextraction.py image_path [output_path]")
         sys.exit(1)
 
+    out_path = "." if len(sys.argv) < 3 else sys.argv[2]  # this directory should already exist
+
     try:
-        mask_path = "__temp-" + os.path.basename(sys.argv[1]).split('.')[0] + "-mask_images"
+        base_path = os.path.join(out_path, os.path.basename(sys.argv[1]).split('.')[0])
+        last_run = sorted(os.listdir(base_path), reverse=True)[0]
+        mask_path = os.path.join(base_path, last_run, "mask_images")
         mask_images = [cv2.imread(_, flags=cv2.IMREAD_GRAYSCALE) for _ in sorted(glob.glob(mask_path + "/*.png"))]
     except:
         print("Unable to find mask images. Did you run maskcreation.py?")
@@ -85,7 +89,7 @@ if __name__ == "__main__":
         Parallel(n_jobs=cpu_count())(
             delayed(extract_features)(*_) for _ in tqdm(zip(image_stack, mask_images)))
 
-    write_path = "__temp-" + os.path.basename(sys.argv[1]).split('.')[0] + "-extracted_features.pickle"
+    write_path = os.path.join(base_path, last_run, "extracted_features.pickle")
     print("Writing extracted features to " + write_path + ".")
     with open(write_path, "wb") as outfile:
         pickle.dump(result, outfile)

src/initialize_kmeans.sh
@@ -13,23 +13,25 @@ module load python/anaconda3.6
 
 source activate celltrack
 
+output="/projects/b1042/Abazeed_Lab/Pete_Priyanka/output"
+
 cd ~/celltrack/src
 
 input="manifest.txt"
 
 while read path; do
   echo "Running maskcreation.py for $path."
-  python3 maskcreation.py $path
+  python3 maskcreation.py $path $output
   if [ $? -eq 1 ]; then
     echo "Failed mask creation. Aborting."
     exit
   fi
   echo "Running ftextraction.py for $path."
-  python3 ftextraction.py $path
+  python3 ftextraction.py $path $output
   if [ $? -eq 1 ]; then
     echo "Failed feature extraction. Aborting."
     exit
   fi
 done < $input
 
-python3 kmeans_initialize.py $input
+python3 kmeans_initialize.py $input $output

commit b8e2109064231eddeb278aa8a6661c3d29232307

Fix bugs introduced in 7441c7a
This pipeline has now been verified to work on Quest.

src/kmeans_initialize.py
@@ -39,8 +39,8 @@ if __name__ == "__main__":
     scaled = scaler.fit_transform(full_df)
     dft = pd.DataFrame(pca.fit_transform(scaled), index=full_df.index)
 
-    if len(sys.argv) > 2:
-        k = sys.argv[2]
+    if len(sys.argv) > 3:
+        k = int(sys.argv[3])
         print("Training kmeans model with k=%i." % k)
         kmeans = KMeans(n_clusters=k, random_state=0)
         kmeans.fit(dft)

src/kmeans_predict.py
@@ -3,13 +3,15 @@ import pandas as pd
 import pickle
 import sys
 
+from tqdm import tqdm
+
 if __name__ == "__main__":
     out_path = "." if len(sys.argv) < 3 else sys.argv[2]  # this directory should already exist
 
     try:
         base_path = os.path.join(out_path, "kmeans")
         last_run = sorted(os.listdir(base_path), reverse=True)[0]
-        kmeans_path = os.path.join(base_path, last_run + ".pickle")
+        kmeans_path = os.path.join(base_path, last_run)  # ".pickle" is contained within last_run
 
         base_path = os.path.join(out_path, os.path.basename(sys.argv[1]).split('.')[0])
         last_run = sorted(os.listdir(base_path), reverse=True)[0]
@@ -36,7 +38,7 @@ if __name__ == "__main__":
         sys.exit(1)
 
     labels = []
-    for df, _ in result:
+    for df, _ in tqdm(result):
         df = pd.DataFrame(pca.transform(scaler.transform(df)), index=df.index)
         labels += [kmeans.predict(df),]
 

src/maskcreation.py
@@ -223,7 +223,7 @@ if __name__ == "__main__":
             delayed(get_mask_image_with_refined_offset)(_) for _ in tqdm(image_stack))
 
     out_path = "." if len(sys.argv) < 3 else sys.argv[2]  # this directory should already exist
-    base_path = os.path.join(outpath, os.path.basename(sys.argv[1]).split('.')[0])
+    base_path = os.path.join(out_path, os.path.basename(sys.argv[1]).split('.')[0])
     if not os.path.exists(base_path):
         print("Creating base directory %s." % base_path)
         os.mkdir(base_path)