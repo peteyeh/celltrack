d535683ca4b74102a367083ed8ba7cc66b128249
Pete Yeh <peteyeh8@gmail.com>
2022-04-18 14:40:26 -0500

    Add script for creation of contoured images

---
+++ src/contour_images.py
@@ -0,0 +1,60 @@
+import cv2
+import imageio
+import os
+import pickle
+import sys
+
+from tqdm import tqdm
+
+from colormap import get_colormap
+from displaytools import get_contoured_image
+from improcessing import scale_image
+
+if __name__ == "__main__":
+    out_path = "." if len(sys.argv) < 3 else sys.argv[2]  # this directory should already exist
+
+    try:
+        base_path = os.path.join(out_path, os.path.basename(sys.argv[1]).split('.')[0])
+        last_run = sorted(os.listdir(base_path), reverse=True)[0]
+        mask_path = os.path.join(base_path, last_run, "extracted_features.pickle")
+        label_path = os.path.join(base_path, last_run, "labels.pickle")
+    except:
+        print("Received invalid image_path. Make sure you execute with:")
+        print("  python3 cluster_analysis.py image_path [output_path]")
+        sys.exit(1)
+
+    try:
+        image_stack = list(map(scale_image, cv2.imreadmulti(sys.argv[1], flags=cv2.IMREAD_GRAYSCALE)[1]))
+        if len(image_stack) == 0:
+            raise Exception()
+    except:
+        print("Unable to read image stack. Make sure you execute with:")
+        print("  python3 maskcreation.py image_path [output_path]")
+        sys.exit(1)
+
+    try:
+        with open(mask_path, "rb") as infile:
+            mask_result = pickle.load(infile)
+            print("Loaded feature extraction data from %s." % mask_path)
+    except:
+        print("Unable to load feature extraction data from %s. Did you run ftextract.py?" % mask_path)
+        sys.exit(1)
+
+    try:
+        with open(label_path, "rb") as infile:
+            label_result = pickle.load(infile)
+            print("Loaded labels from %s." % label_path)
+    except:
+        print("Unable to load labels from %s. Did you run kmeans_predict.py?" % label_path)
+        sys.exit(1)
+
+    if len(image_stack) != len(mask_result) or len(image_stack) != len(label_result):
+    	print("Unable to match image stack to saved mask and label data. Aborting.")
+    	sys.exit(1)
+
+    write_path = os.path.join(base_path, last_run, "contoured_images")
+    print("Writing mask images to " + write_path + ":")
+    os.mkdir(write_path)
+    for i in tqdm(range(len(image_stack))):
+        imageio.imwrite((write_path + "/%i.png" % i),
+                        get_contoured_image(image_stack[i], mask_result[i][1], label_result[i], get_colormap()))
