65fa1b91dcf6ec8aae08117547bc23acc8aa1e2a
Pete Yeh <peteyeh8@gmail.com>
2022-05-06 21:56:58 -0400

    Add script for lightweight exporting
    The contoured images and labeled data are quite bulky and do add up
    (~1GB per image stack), so let's create a separate script that will
    only package the essentials.

---
+++ src/package_output_light.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+output="/projects/p31689/Image_Analysis/output"
+input="manifest.txt"
+
+saved_time=$(date +"%Y%m%d%H%M%S")
+
+package_path=$output/packaged_output_light/$saved_time
+mkdir $package_path
+
+while read path; do
+  filename=$(basename $path)
+  filename="${filename%.*}"
+  output_path=$output/$filename/$(ls $output/$filename | tail -n 1)
+  echo "Copying from $output_path..."
+  echo " ...to $package_path/$filename"
+  mkdir $package_path/$filename
+  cp $output_path/analysis.csv $package_path/$filename
+  cp $output_path/*.png $package_path/$filename
+  cp -r $output_path/mask_images $package_path/$filename
+done < $input
